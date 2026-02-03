6a35df0d1bd74fdd6ac6a5fb99a03bf8bac5cf0e
David Iya <IYA@Davids-MBP.lan>
2026-02-03 10:35:36 -0600

    Revert "Convert to The First Bakery School by Marie Temby"
    This reverts commit 348ef193bbdead7b52fd3262b30a8df1ae85a492.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import { resolve } from "path";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  turbopack: {
-    root: resolve(import.meta.dirname!),
-  },
 };
 
 export default nextConfig;
